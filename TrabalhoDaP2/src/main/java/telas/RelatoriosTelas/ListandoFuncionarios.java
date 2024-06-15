5af1ff0d98dbd581ae9e9fab01d95adb53c4b30e
zPolga <adrianpolga@gmail.com>
2024-06-15 01:28:23 -0300

    TelasFaltantes

--- TrabalhoDaP2/src/main/java/telas/RelatoriosTelas/ListandoFuncionarios.java
+++ TrabalhoDaP2/src/main/java/telas/RelatoriosTelas/ListandoFuncionarios.java
@@ -1,17 +1,26 @@
 package telas.RelatoriosTelas;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.LinkedHashMap;
 
 public class ListandoFuncionarios extends JFrame{
     private JTable nome;
-    String []Colunas = {"ID","Nome","CPF","RG","Data de Nascimento","Sexo","Email","Data Admissão","Cargo","Função"};
 
     public void configurar() {
+        String []Colunas = {"ID","Nome","CPF","RG","Data de Nascimento","Sexo","Email","Data Admissão","Cargo","Função"};
         setTitle("Relatório Funcionário");
         setSize(800, 700);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
+        Object[][] Linhas = {{"190","AdrianP","123","321","12/12/2112","M","adrian@gmail.com","12/12/2112","Developer","Developar"},{"190","AdrianP","123","321","12/12/2112","M","adrian@gmail.com","12/12/2112","Developer","Developar"}};
+        DefaultTableModel NomeColunas = new DefaultTableModel(Linhas,Colunas);
+        nome = new JTable(NomeColunas);
+        add(nome);
+
+
+
     }
 
     public ListandoFuncionarios(){
